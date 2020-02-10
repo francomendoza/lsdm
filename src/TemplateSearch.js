e732fc4dcabc9ca01a0553cbf0c659de1854e05f
francomendoza <frm210@gmail.com>
2020-02-10 18:05:28 -0500

    test out styled components

--- src/TemplateSearch.js
+++ src/TemplateSearch.js
@@ -2,22 +2,25 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { createAndViewTemplateInstance } from "./actions/templateActions";
 import { createTemplateInstance } from "./api/templateInstances";
+import styled from "styled-components";
 
 const TemplateSearch = props => {
   let [query, updateQuery] = useState("");
 
   return (
     <div>
-      <input
+      <Search
         type="text"
         value={query}
         onChange={event => updateQuery(event.target.value)}
       />
       {props.templateIds
-        .filter(id => props.templatesById[id].name.includes(query))
+        .filter(id =>
+          props.templatesById[id].name.toLowerCase().includes(query)
+        )
         .map(templateId => {
           return (
-            <div
+            <Result
               key={templateId}
               onClick={() => {
                 let templateInstance = createTemplateInstance(templateId);
@@ -26,12 +29,26 @@ const TemplateSearch = props => {
               }}
             >
               {props.templatesById[templateId].name}
-            </div>
+            </Result>
           );
         })}
     </div>
   );
 };
+
+const Search = styled.input`
+  margin: 20px 0;
+  font-size: 20px;
+  width: 100%;
+`;
+
+const Result = styled.div`
+  margin-top: 10px;
+  cursor: pointer;
+  &:hover {
+    border-bottom: 2px solid var(--dark-green);
+  }
+`;
 
 const mapStateToProps = state => {
   return {
