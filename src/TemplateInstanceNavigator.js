1d30289592a7228abe82be520926d7c986adb4ab
francomendoza <frm210@gmail.com>
2020-05-04 22:17:49 -0400

    add graph path to route

--- src/TemplateInstanceNavigator.js
+++ src/TemplateInstanceNavigator.js
@@ -1,22 +1,47 @@
 import React from "react";
 import { Link } from "@reach/router";
 
-const TemplateInstanceNavigator = props => {
+/*
+Ideally this would work by keeping track of where you are in the subtree
+as you click deeper into new nodes.
+
+When you navigate with browser buttons...
+click back...
+user should be taken to the previous instance with the most recent instance removed
+click forward...
+user should be taken to the new instance with the breadcrumb put back
+
+when you navigate with breadcrumb...
+you should see all the following breadcrumbs 
+templateInstance/:id?path=id,id
+ */
+
+const TemplateInstanceNavigator = (props) => {
   return (
     <nav>
       <ol>
-        {props.templateGraphPath.map(node => {
-          return <Breadcrumb key={node.id} templateInstance={node} />;
+        {props.templateGraphPath.map((node, i) => {
+          return (
+            <Breadcrumb
+              key={node.id}
+              templateInstance={node}
+              path={props.templateGraphPath.slice(0, i + 1)}
+            />
+          );
         })}
       </ol>
     </nav>
   );
 };
 
-const Breadcrumb = props => {
+const Breadcrumb = (props) => {
   return (
     <li>
-      <Link to={`/template_instances/${props.templateInstance.id}`}>
+      <Link
+        to={`/template_instances/${
+          props.templateInstance.id
+        }?graphPath=${props.path.map((node) => node.id).join(",")}`}
+      >
         <span>{props.templateInstance.name}</span>
       </Link>
     </li>
